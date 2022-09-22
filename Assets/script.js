8cb4f02ef09b0c65130b0b6c9ab443b18d7be23c
Joseph Lamb <jlambpco@gmail.com>
2022-09-22 09:46:14 -0400

    first commit

---
+++ Assets/script.js
@@ -0,0 +1,14 @@
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+generateBtn.addEventListener("click", writePassword);
+ 
